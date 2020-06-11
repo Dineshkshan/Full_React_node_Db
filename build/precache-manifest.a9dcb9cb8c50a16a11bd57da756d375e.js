30436f817e6775e7ef2a125f7e996805150e44d2
Dineshkshan <dineshk.shan@gmail.com>
2020-06-11 14:12:46 +0530

    changes made in package.json

---
+++ build/precache-manifest.a9dcb9cb8c50a16a11bd57da756d375e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3102775056a2363ba15546c7d22240",
+    "url": "/index.html"
+  },
+  {
+    "revision": "412ff929b0e7e77795e2",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d2988e9c34bb09321e03",
+    "url": "/static/css/main.9809718b.chunk.css"
+  },
+  {
+    "revision": "412ff929b0e7e77795e2",
+    "url": "/static/js/2.d192eade.chunk.js"
+  },
+  {
+    "revision": "9985bc8723f69546fd42cf716c44bc97",
+    "url": "/static/js/2.d192eade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2988e9c34bb09321e03",
+    "url": "/static/js/main.12e4c9b6.chunk.js"
+  },
+  {
+    "revision": "b00eda10b8f49cf81f85",
+    "url": "/static/js/runtime-main.f371e43a.js"
+  }
+]);
